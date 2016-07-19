79685d9700045a333166bc1a7a2570b650e360fe
Lorenz Hübschle-Schneider <huebschle@kit.edu>
2016-07-19 11:12:12 +0200

    parameter range in run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 U='2^31'
-size=1000
 iterations=100
 
 # sequential
-for logsize in {10..30}; do
+for logsize in {5..27}; do
     echo "==============================="
     echo "Running with 2^$logsize samples"
     size=$((2**$logsize))
-    ratio=$(echo "$size / ($U)" | bc -l)
+    ratio=$(echo "$size / ($U)" | bc -l) # shell math is meh
     ./rand -t 1 -s $size -p $ratio -i $iterations $@
     echo "==============================="
     echo ""
