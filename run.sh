d4d1d98f4c96c3947b58857559d28eae708cf75c
Lorenz Hübschle-Schneider <huebschle@kit.edu>
2016-08-12 11:06:01 +0200

    Runner: use pgo if available

--- run.sh
+++ run.sh
@@ -2,14 +2,20 @@
 
 universe=$((2**30))
 
+SUFF=
+if [[ -f rand32-pgo && -f rand32S-pgo && -f rand64-pgo && -f rand64S-pgo ]]; then
+    echo "Using PGO'd binaries"
+    SUFF=-pgo
+fi
+
 # sequential
 for logsamples in {7..27}; do
     echo "==============================================="
     samples=$((2**$logsamples))
     iterations=$((2**30 / $samples))
     echo "Running with 2^$logsamples samples ($iterations iterations)"
-    time ./rand32-pgo -q -t 1 -k $samples -n $universe -i $iterations $@
-    time ./rand32S-pgo -q -t 1 -k $samples -n $universe -i $iterations $@
+    time ./rand32$SUFF -q -t 1 -k $samples -n $universe -i $iterations $@
+    time ./rand32S$SUFF -q -t 1 -k $samples -n $universe -i $iterations $@
     echo "==============================================="
     echo ""
 done
@@ -30,8 +36,8 @@ for logsamples in {7..27}; do
     samples=$((2**$logsamples))
     iterations=$((2**30 / $samples))
     echo "Running with 2^$logsamples samples ($iterations iterations)"
-    time ./rand64-pgo -q -t 1 -k $samples -n $universe -i $iterations $@
-    time ./rand64S-pgo -q -t 1 -k $samples -n $universe -i $iterations $@
+    time ./rand64$SUFF -q -t 1 -k $samples -n $universe -i $iterations $@
+    time ./rand64S$SUFF -q -t 1 -k $samples -n $universe -i $iterations $@
     echo "==============================================="
     echo ""
 done
